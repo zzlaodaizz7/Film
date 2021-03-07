3ade7d20147b762c035f66b39bff5fd7ff98f456
Nguyễn Ngọc Khang <khangnn@appota.com>
2021-03-07 22:18:40 +0700

    #2 detail

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,17 @@ use App\Http\Controllers\Backend\NationController;
 use App\Http\Controllers\Backend\FilmController;
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\Frontend\DetailController;
 
-Route::middleware(['auth'])->group(function () {
+Route::get('{code}', [DetailController::class, 'index']);
+
+Auth::routes();
+
+Route::group(['prefix' => 'admin'], function () {
     Route::resource('category', CategoryController::class);
     Route::resource('nation', NationController::class);
     Route::resource('film', FilmController::class);
-    Route::get('/home', 'HomeController@index')->name('home');
+//    Route::get('/home', 'HomeController@index')->name('home');
 });
-
-
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
